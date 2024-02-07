4aeb8927ec6d740db17dcfe2907b37f951416fc7
Buyaki01 <rittahbuyaki@gmail.com>
2024-02-07 08:08:37 +0300

    connect to mongoDB

--- api/index.js
+++ api/index.js
@@ -1,6 +1,15 @@
 import express from 'express'; // Add "type": "module", in package.json to allow one to use import instead of require
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const app = express()
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((err) => {
+  console.log(err);
+})
+
+const app = express();
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
