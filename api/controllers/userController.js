27469bb4419de6554e86529394559152af576f32
Buyaki01 <rittahbuyaki@gmail.com>
2024-02-08 15:18:31 +0300

    Add hashedPassword

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,13 +1,16 @@
 import User from "../models/User.js ";
+import bcryptjs from 'bcryptjs';
 
 export const register = async (req, res) => {
   const {username, email, password} = req.body;
+
+  const hashedPassword = bcryptjs.hashSync(password, 10);
 
   try {
     const user = await User.create({
       username,
       email,
-      password,
+      password: hashedPassword,
     });
   
     res.status(201).json({ message: "User created successfully" });
