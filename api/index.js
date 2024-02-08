fe15bae5aac131e3cde3ae95d551a4c7b707a857
Buyaki01 <rittahbuyaki@gmail.com>
2024-02-08 13:36:06 +0300

    Add test api route

--- api/index.js
+++ api/index.js
@@ -1,16 +1,21 @@
+import dotenv from 'dotenv';
+dotenv.config({ path: '../.env' });
 import express from 'express'; // Add "type": "module", in package.json to allow one to use import instead of require
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-dotenv.config();
+import userRoutes from './routes/userRoutes.js';
 
-mongoose.connect(process.env.MONGODB_URI).then(() => {
-  console.log('Connected to MongoDB');
-}).catch((err) => {
-  console.log(err);
-})
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('Connected to MongoDB')
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error.message)
+  });
 
 const app = express();
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
 })
+
+app.use("/api/user", userRoutes)
